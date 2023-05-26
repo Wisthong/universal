8ca76d623db0d184781166c745616a8c796ee650
Wisthong Martinez <wisthongd@gmail.com>
2023-05-26 10:54:36 -0500

    init project

---
+++ src/app/modules/cliente/forms/forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css'],
+})
+export default class FormsComponent {}

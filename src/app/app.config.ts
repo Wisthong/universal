487eb0a154b9f1df899562d3a45222c8af53d199
Wisthong Martinez <wisthongd@gmail.com>
2023-05-26 12:31:04 -0500

    Formulario

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),importProvidersFrom(HttpClientModule) ]
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), provideAnimations()]
 };
--- src/app/modules/cliente/forms/forms.component.ts
+++ src/app/modules/cliente/forms/forms.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css'],
 })
